d0c324ececc788c807eeb857e7a019cbcf2b8ae9
Insha Ramin <130788673+Insharamin12@users.noreply.github.com>
2024-02-13 10:49:44 +0530

    Update main.py
    Created a list of list for the cells

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import random, time, copy
 WIDTH = 60
 HEIGHT = 20
+
+# Create a list of list for the cells:
+nextCells = []
+for x in range(WIDTH):
+    column = [] # Create a new column.
+    for y in range(HEIGHT):
+        if random.randint(0, 1) == 0:
+            column.append('#') # Add a living cell.
+        else:
+            column.append(' ') # Add a dead cell.
+    nextCells.append(column) # nextCells is a list of column lists.
